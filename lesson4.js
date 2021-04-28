4399ed3a645b6c349823a1eb65accb6f8b73d650
EfratRozner <r0533103503@gmail.com>
2021-04-28 23:11:34 +0300

    efrat3

--- lesson4.js
+++ lesson4.js
@@ -3,6 +3,6 @@ var birthday;
     birthday[birthday["תשרי"] = 0] = "תשרי";
     birthday[birthday["חשוון"] = 1] = "חשוון";
     birthday[birthday["כסלו"] = 2] = "כסלו";
-    birthday[birthday["טבת"] = 3] = "טבת";
+    birthday[birthday["וטאוטאכעאטעעחל"] = 3] = "טבת";
     birthday[birthday["שבכגעגט"] = 4] = "שבט";
 })(birthday || (birthday = {}));
